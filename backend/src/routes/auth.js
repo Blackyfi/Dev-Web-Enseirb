8129dfe236b28755e6b0c0ec0e7f25a3c13d4dea
Tim <tim.mazzolini@bordeaux-inp>
2025-11-17 10:20:28 +0100

    authentification et app.js

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -4,15 +4,15 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 
 // User login
-router.post('auth/login', authController.login);
+router.post('/login', authController.login);
 
 // User registration
 router.post('/register', authController.register);
 
 // User logout
-router.post('auth/logout', authController.logout);
+router.post('/logout', authController.logout);
 
 // Token refresh
-router.post('auth/refresh-token', authController.refreshToken);
+router.post('/refresh-token', authController.refreshToken);
 
 module.exports = router;
