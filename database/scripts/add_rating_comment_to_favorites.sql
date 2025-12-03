9a1876b4939cf15df1751ae62f74a9326ac4f22a
Tim <mazzolinitim@gmail.com>
2025-12-03 12:00:29 +0100

    Fin dockerisation

--- database/scripts/add_rating_comment_to_favorites.sql
+++
@@ -1,9 +0,0 @@
--- Migration: Ajout des colonnes rating et comment à la table favorites
--- Date: 2025-12-03
-
-USE seenflix;
-
--- Ajouter les colonnes rating et comment à la table favorites
-ALTER TABLE favorites
-ADD COLUMN rating INT DEFAULT NULL CHECK (rating >= 1 AND rating <= 5),
-ADD COLUMN comment TEXT DEFAULT NULL;
